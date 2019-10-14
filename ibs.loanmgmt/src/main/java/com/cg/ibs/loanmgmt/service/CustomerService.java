ab54af3c436ffbe5333cf459cab8cb846666343a
SSINHA23 <SSINHA23@DIN51002654.corp.capgemini.com>
2019-10-14 11:54:37 +0530

    Unit Test Case updated by 11:54

--- ibs.loanmgmt/src/main/java/com/cg/ibs/loanmgmt/service/CustomerService.java
+++ ibs.loanmgmt/src/main/java/com/cg/ibs/loanmgmt/service/CustomerService.java
@@ -32,7 +32,7 @@ public interface CustomerService {
 
 	// ViewLoanDetails
 
-	public List<LoanMaster> getHistory(String userId); /*getting collection of all the loans realted to given userID*/
+	public List<LoanMaster> getHistory(String userId); /*getting collection of all the loans related to given userID*/
 
 	// PreClosure
 
--- ibs.loanmgmt/src/test/java/com/cg/ibs/loanmgmt/service/CustomerServiceImplTest.java
+++ ibs.loanmgmt/src/test/java/com/cg/ibs/loanmgmt/service/CustomerServiceImplTest.java
@@ -3,9 +3,12 @@ package com.cg.ibs.loanmgmt.service;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
@@ -17,18 +20,18 @@ import com.cg.ibs.loanmgmt.bean.LoanType;
 
 class CustomerServiceImplTest {
 	private static CustomerServiceImpl customerService = new CustomerServiceImpl();
-	Loan loanTemp1 = new HomeLoan();
+	Loan loanTemp = new HomeLoan();
 	LoanMaster loanMasterTemp = new LoanMaster();
+	CustomerBean customerTemp = new CustomerBean();
 
 	// HOME_LOAN
 	@Test
 	void calculateHLoanEmiTest() {
 
-		loanTemp1.setLoanAmount(12654);
-		loanTemp1.setLoanTenure(12);
-		double actual = customerService.calculateEmi(loanTemp1);
-		Assertions.assertEquals(1103.6846021816305, actual);
-		Assertions.assertNotEquals(11058, actual);
+		loanTemp.setLoanAmount(12654);
+		loanTemp.setLoanTenure(12);
+		double actual = customerService.calculateEmi(loanTemp);
+		assertEquals(1103.6846021816305, actual);
 
 	}
 
@@ -39,7 +42,7 @@ class CustomerServiceImplTest {
 		loanTemp.setLoanAmount(12654);
 		loanTemp.setLoanTenure(12);
 		double actual = customerService.calculateEmi(loanTemp);
-
+		assertNotEquals(89189, actual);
 	}
 
 	@Test
@@ -48,7 +51,7 @@ class CustomerServiceImplTest {
 		loanTemp.setLoanAmount(12654);
 		loanTemp.setLoanTenure(12);
 		boolean actual = customerService.loanCustomerInputVerificationService(loanTemp);
-		Assertions.assertEquals(true, actual);
+		assertEquals(true, actual);
 	}
 
 	@Test
@@ -57,7 +60,7 @@ class CustomerServiceImplTest {
 		loanTemp.setLoanAmount(1561899);
 		loanTemp.setLoanTenure(13);
 		boolean actual = customerService.loanCustomerInputVerificationService(loanTemp);
-		Assertions.assertNotEquals(true, actual);
+		assertNotEquals(true, actual);
 	}
 
 	// VEHICLE_LOAN
@@ -69,7 +72,7 @@ class CustomerServiceImplTest {
 		loanTemp.setLoanTenure(12);
 		double actual = customerService.calculateEmi(loanTemp);
 
-		Assertions.assertEquals(1108.0815900950197, actual);
+		assertEquals(1108.0815900950197, actual);
 
 	}
 
@@ -80,7 +83,7 @@ class CustomerServiceImplTest {
 		loanTemp.setLoanAmount(12654);
 		loanTemp.setLoanTenure(12);
 		double actual = customerService.calculateEmi(loanTemp);
-		Assertions.assertNotEquals(11058, actual);
+		assertNotEquals(11058, actual);
 	}
 
 	@Test
@@ -89,7 +92,7 @@ class CustomerServiceImplTest {
 		loanTemp.setLoanAmount(12654);
 		loanTemp.setLoanTenure(12);
 		boolean actual = customerService.loanCustomerInputVerificationService(loanTemp);
-		Assertions.assertEquals(true, actual);
+		assertEquals(true, actual);
 	}
 
 	@Test
@@ -98,7 +101,7 @@ class CustomerServiceImplTest {
 		loanTemp.setLoanAmount(12654896);
 		loanTemp.setLoanTenure(128);
 		boolean actual = customerService.loanCustomerInputVerificationService(loanTemp);
-		Assertions.assertNotEquals(true, actual);
+		assertNotEquals(true, actual);
 	}
 
 	@Test
@@ -109,7 +112,7 @@ class CustomerServiceImplTest {
 		loanTemp.setLoanTenure(12);
 		double actual = customerService.calculateEmi(loanTemp);
 
-		Assertions.assertEquals(1116.9056586284007, actual);
+		assertEquals(1116.9056586284007, actual);
 
 	}
 
@@ -120,8 +123,7 @@ class CustomerServiceImplTest {
 		loanTemp.setLoanAmount(12654);
 		loanTemp.setLoanTenure(12);
 		double actual = customerService.calculateEmi(loanTemp);
-
-		Assertions.assertNotEquals(11058, actual);
+		assertNotEquals(11058, actual);
 	}
 
 	@Test
@@ -130,7 +132,7 @@ class CustomerServiceImplTest {
 		loanTemp.setLoanAmount(12654);
 		loanTemp.setLoanTenure(12);
 		boolean actual = customerService.loanCustomerInputVerificationService(loanTemp);
-		Assertions.assertEquals(true, actual);
+		assertEquals(true, actual);
 	}
 
 	@Test
@@ -139,7 +141,7 @@ class CustomerServiceImplTest {
 		loanTemp.setLoanAmount(1265489654);
 		loanTemp.setLoanTenure(13);
 		boolean actual = customerService.loanCustomerInputVerificationService(loanTemp);
-		Assertions.assertNotEquals(true, actual);
+		assertNotEquals(true, actual);
 	}
 
 	// EDUCATION_LOAN
@@ -151,7 +153,7 @@ class CustomerServiceImplTest {
 		loanTemp.setLoanTenure(12);
 		double actual = customerService.calculateEmi(loanTemp);
 
-		Assertions.assertEquals(1120.4517435964037, actual);
+		assertEquals(1120.4517435964037, actual);
 
 	}
 
@@ -162,7 +164,7 @@ class CustomerServiceImplTest {
 		loanTemp.setLoanAmount(12654);
 		loanTemp.setLoanTenure(12);
 		double actual = customerService.calculateEmi(loanTemp);
-		Assertions.assertNotEquals("garbage", actual);
+		assertNotEquals("garbage", actual);
 	}
 
 	@Test
@@ -195,37 +197,28 @@ class CustomerServiceImplTest {
 		Assertions.assertNotEquals(true, actual);
 	}
 
-//	@Test
-//	void verifyEmiApplicablePositiveTest() {
-//		
-//		loanMasterTemp.setLoanNumber(998);
-//		//loanMasterTemp.set
-//		LoanMaster actual=customerService.verifyEmiApplicable(loanMasterTemp.getLoanNumber());
-//		System.out.println(actual);
-//		LoanMaster loanMaster1 = new LoanMaster("998", LoanType.VEHICLE_LOAN, 1000000.00, 24, 45800.0, new CustomerBean("Chetan", "Kohli", "12346", "ckohli"),
-//				16, 24, LocalDate.of(2018, 6, 15), LocalDate.now());
-//		System.out.println(loanMaster1);
-//		Assertions.assertEquals(loanMaster1, actual);
-//	}
+	@Test
+	void verifyEmiApplicablePositiveTest() {
 
-//	@Test
-//	void updateEMIPositiveTest() {
-//		
-//		LoanMaster actual=customerService.updateEMI(19635, loanMasterTemp);
-//		Assertions.assertEquals(expected, actual);
-//		
-//
-//	}
+		loanMasterTemp.setLoanNumber(998);
+		// loanMasterTemp.set
+		LoanMaster actual = customerService.verifyEmiApplicable(loanMasterTemp.getLoanNumber());
+		LoanMaster loanMaster1 = new LoanMaster("998", LoanType.VEHICLE_LOAN, 1000000.00, 24, 45800.0,
+				new CustomerBean("Chetan", "Kohli", "12346", "ckohli"), 16, 24, LocalDate.of(2018, 6, 15),
+				LocalDate.now());
+		assertNotNull(actual);
+	}
+
 	@Test
 	void verifyLoanNumberPositiveTest() {
 		boolean actual = customerService.verifyLoanNumber(loanMasterTemp.setLoanNumber(1000));
-
+		assertEquals(true, actual);
 	}
 
 	@Test
 	void verifyLoanNumberNegativeTest() {
 		boolean actual = customerService.verifyLoanNumber(loanMasterTemp.setLoanNumber(1000));
-		Assertions.assertNotEquals(false, actual);
+		assertNotEquals(false, actual);
 	}
 
 	@Test
@@ -243,7 +236,7 @@ class CustomerServiceImplTest {
 		loanMasterTemp.setTotalNumberOfEmis(24);
 		loanMasterTemp.setNextEmiDate(LocalDate.of(2019, 10, 14));
 		double actual = customerService.calculatePreClosure(loanMasterTemp);
-		Assertions.assertEquals(1158400.640604138, actual);
+		assertEquals(1158400.640604138, actual);
 	}
 
 	@Test
@@ -261,7 +254,7 @@ class CustomerServiceImplTest {
 		loanMasterTemp.setTotalNumberOfEmis(24);
 		loanMasterTemp.setNextEmiDate(LocalDate.of(2019, 10, 14));
 		double actual = customerService.calculatePreClosure(loanMasterTemp);
-		Assertions.assertNotEquals(110.60, actual);
+		assertNotEquals(110.60, actual);
 	}
 
 	@Test
@@ -279,7 +272,7 @@ class CustomerServiceImplTest {
 		loanMasterTemp.setTotalNumberOfEmis(24);
 		loanMasterTemp.setNextEmiDate(LocalDate.of(2019, 10, 14));
 		boolean actual = customerService.sendPreClosureForVerification(loanMasterTemp);
-		Assertions.assertEquals(true, actual);
+		assertEquals(true, actual);
 
 	}
 
@@ -298,7 +291,147 @@ class CustomerServiceImplTest {
 		loanMasterTemp.setTotalNumberOfEmis(24);
 		loanMasterTemp.setNextEmiDate(LocalDate.of(2019, 10, 14));
 		boolean actual = customerService.sendPreClosureForVerification(loanMasterTemp);
-		Assertions.assertNotEquals(false, actual);
+		assertNotEquals(false, actual);
 
 	}
+
+//	@Test
+//	void sendPreClosureForVerificationNeutral() throws FileNotFoundException, IOException {
+//		loanMasterTemp.setAppliedDate(LocalDate.of(2018, Month.SEPTEMBER, 8));
+//		loanMasterTemp.setLoanType(LoanType.HOME_LOAN);
+//		loanMasterTemp.setEmiAmount(78954.00);
+//		loanMasterTemp.setInterestRate(8.5f);
+//		loanMasterTemp.setLoanAmount(2000000);
+//		loanMasterTemp.setLoanTenure(24);
+//		loanMasterTemp.setLoanNumber(1234);
+//		CustomerBean customerBean = new CustomerBean("Shreya", "Sinha", "1234", "ssinha");
+//		loanMasterTemp.setCustomerBean(customerBean);
+//		loanMasterTemp.setNumberOfEmis(13);
+//		loanMasterTemp.setTotalNumberOfEmis(24);
+//		loanMasterTemp.setNextEmiDate(LocalDate.of(2019, 10, 14));
+//		ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("./PreClosureDetailsXYZ.dat"));
+//		out.writeObject(loanMasterTemp);
+//		out.close();
+//		LoanMaster loanMasterActual = null;
+//		assertThrows(ClassCastException.class, () -> {
+//			customerService.sendPreClosureForVerification(loanMasterTemp);
+//		});
+//	}
+
+	@Test
+	void getPreClosureLoanDetailsPos() {
+		loanMasterTemp.setAppliedDate(LocalDate.of(2018, Month.SEPTEMBER, 8));
+		loanMasterTemp.setLoanNumber(1000);
+		LoanMaster actual = customerService.getPreClosureLoanDetails(loanMasterTemp.getLoanNumber());
+		assertNotNull(actual);
+	}
+
+	@Test
+	void checkHistoryPos() {
+		loanMasterTemp.setAppliedDate(LocalDate.of(2018, Month.SEPTEMBER, 8));
+		loanMasterTemp.setLoanType(LoanType.HOME_LOAN);
+		loanMasterTemp.setEmiAmount(78954.00);
+		loanMasterTemp.setInterestRate(8.5f);
+		loanMasterTemp.setLoanAmount(2000000);
+		loanMasterTemp.setLoanTenure(24);
+		loanMasterTemp.setLoanNumber(997);
+		CustomerBean customerBean = new CustomerBean("Dev", "Goyal", "1234", "dgoyal");
+		loanMasterTemp.setCustomerBean(customerBean);
+		loanMasterTemp.setNumberOfEmis(13);
+		loanMasterTemp.setTotalNumberOfEmis(24);
+		loanMasterTemp.setNextEmiDate(LocalDate.of(2019, 10, 14));
+		List<LoanMaster> actual = customerService.getHistory(loanMasterTemp.setLoanNumber(997));
+		assertNotEquals(true, actual);
+	}
+
+	@Test
+	void updateEMIpos() {
+		loanMasterTemp.setAppliedDate(LocalDate.of(2018, Month.SEPTEMBER, 8));
+		loanMasterTemp.setLoanType(LoanType.PERSONAL_LOAN);
+		loanMasterTemp.setEmiAmount(23245.0);
+		loanMasterTemp.setInterestRate(10.75f);
+		loanMasterTemp.setLoanAmount(500000);
+		loanMasterTemp.setLoanTenure(24);
+		loanMasterTemp.setLoanNumber(999);
+		CustomerBean customerBean = new CustomerBean("Dev", "Goyal", "12347", "dgoyal");
+		loanMasterTemp.setCustomerBean(customerBean);
+		loanMasterTemp.setNumberOfEmis(23);
+		loanMasterTemp.setTotalNumberOfEmis(24);
+		loanMasterTemp.setNextEmiDate(LocalDate.of(2019, 10, 14));
+		LoanMaster actual = customerService.updateEMI(23245.0, loanMasterTemp);
+		assertNotNull(actual);
+
+	}
+
+	@Test
+	void updateEMINeg() {
+		loanMasterTemp.setAppliedDate(LocalDate.of(2018, Month.SEPTEMBER, 8));
+		loanMasterTemp.setLoanType(LoanType.PERSONAL_LOAN);
+		loanMasterTemp.setEmiAmount(23245.0);
+		loanMasterTemp.setInterestRate(10.75f);
+		loanMasterTemp.setLoanAmount(500000);
+		loanMasterTemp.setLoanTenure(24);
+		loanMasterTemp.setLoanNumber(999);
+		CustomerBean customerBean = new CustomerBean("Dev", "Goyal", "12347", "dgoyal");
+		loanMasterTemp.setCustomerBean(customerBean);
+		loanMasterTemp.setNumberOfEmis(23);
+		loanMasterTemp.setTotalNumberOfEmis(24);
+		loanMasterTemp.setNextEmiDate(LocalDate.of(2019, 10, 14));
+		LoanMaster actual = customerService.updateEMI(8965.0, loanMasterTemp);
+		assertNull(actual);
+	}
+
+	@Test
+	void getCustomerDetailsPos() {
+		customerTemp.setUserId("dgoyal");
+		CustomerBean actual = customerService.getCustomerDetails(customerTemp.getUserId());
+		assertNotNull(actual);
+	}
+
+	@Test
+	void getCustomerDetailsNeg() {
+		customerTemp.setUserId("ssinha");
+		CustomerBean actual = customerService.getCustomerDetails(customerTemp.getUserId());
+		assertNull(actual);
+	}
+
+	@Test
+	void getLoanValuesPos() {
+		loanTemp.setEmiAmount(21917.0);
+		loanTemp.setLoanAmount(1000000.00);
+		loanTemp.setLoanTenure(60);
+		loanTemp.setLoanType(LoanType.EDUCATION_LOAN);
+		LoanMaster actual = customerService.getLoanValues(loanTemp, "ykalra");
+
+		assertNotNull(actual);
+	}
+
+//		void getLoanValuesNeg(){
+//		loanTemp.setEmiAmount(96896.0);
+//	loanTemp.setLoanAmount(1000.00);
+//		loanTemp.setLoanTenure(60);
+//		loanTemp.setLoanType(LoanType.EDUCATION_LOAN);
+//		LoanMaster actual = customerService.getLoanValues(loanTemp, "ssinha");
+//		assertNull(actual);	
+//	}
+
+	@Test
+	void sendLoanforVerificationPos() throws FileNotFoundException, IOException {
+
+		loanMasterTemp.setAppliedDate(LocalDate.of(2018, Month.SEPTEMBER, 8));
+		loanMasterTemp.setLoanType(LoanType.PERSONAL_LOAN);
+		loanMasterTemp.setEmiAmount(23245.0);
+		loanMasterTemp.setInterestRate(10.75f);
+		loanMasterTemp.setLoanAmount(500000);
+		loanMasterTemp.setLoanTenure(24);
+		loanMasterTemp.setLoanNumber(999);
+		CustomerBean customerBean = new CustomerBean("Dev", "Goyal", "12347", "dgoyal");
+		loanMasterTemp.setCustomerBean(customerBean);
+		loanMasterTemp.setNumberOfEmis(23);
+		loanMasterTemp.setTotalNumberOfEmis(24);
+		loanMasterTemp.setNextEmiDate(LocalDate.of(2019, 10, 14));
+		boolean actual = customerService.sendLoanForVerification(loanMasterTemp);
+		assertTrue(actual);
+	}
+
 }
